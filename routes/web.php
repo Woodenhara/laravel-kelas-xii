54698ee698cc6d7c189dd87e4bc86f2726c03ce9
Fajar abdilah <fajarabdilah516@gmail.com>
2024-07-31 13:04:58 +0700

    Register

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     FilmController,
+    RegisterController
 };
 
 Route::get('/', [FilmController::class, 'movieHome'])->name('home');
 Route::get('/movies', [FilmController::class, 'movies'])->name('movies');
 Route::get('/movies/{film}', [FilmController::class, 'show'])->name('movies.show');
 Route::get('/movies/genre/{genre}', [FilmController::class, 'moviesByGenre'])->name('genre');
-
-Route::middleware(['can:isUser'])->group(function() {
-    Route::resource('/movies', FilmController::class);
+Route::controller(RegisterController::class)->group(function () {
+    Route::get('/register', 'create')->name('register.create');
+    Route::post('/register', 'store')->name('register.store');
 });
+// Route::middleware(['can:isUser'])->group(function() {
+//     Route::resource('/movies', FilmController::class);
+// });
 
