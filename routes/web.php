b685c68cf3ec041bf4426f5c7416c5681d78bfea
dwinurfatimah <118708533+dwinurfatimah20@users.noreply.github.com>
2024-08-01 19:16:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     FilmController,
-    RegisterController
+    RegisterController,
+    AuthController
 };
 
 Route::get('/', [FilmController::class, 'movieHome'])->name('home');
 Route::get('/movies', [FilmController::class, 'movies'])->name('movies');
 Route::get('/movies/{film}', [FilmController::class, 'show'])->name('movies.show');
 Route::get('/movies/genre/{genre}', [FilmController::class, 'moviesByGenre'])->name('genre');
+
 Route::controller(RegisterController::class)->group(function () {
     Route::get('/register', 'create')->name('register.create');
     Route::post('/register', 'store')->name('register.store');
 });
-// Route::middleware(['can:isUser'])->group(function() {
-//     Route::resource('/movies', FilmController::class);
-// });
 
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/login', 'login')->name('login');
+    Route::post('/login', 'authenticate')->name('login.authenticate');
+    Route::post('/logout', 'logout')->name('logout');
+});
